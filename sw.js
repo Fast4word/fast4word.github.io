81fca65c52d9e8bc512ee05e817c92166bb85352
Fast4word <152832715+Fast4word@users.noreply.github.com>
2026-03-07 09:45:26 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,8 @@ const OFFLINE_URL = '/offline.html';
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.add(OFFLINE_URL);
+      // Added {cache: 'reload'} to ensure you aren't caching a stale version
+      return cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
     })
   );
   self.skipWaiting();
@@ -26,12 +27,12 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
     event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match(OFFLINE_URL);
+      fetch(event.request).catch(async () => {
+        const cache = await caches.open(CACHE_NAME);
+        return await cache.match(OFFLINE_URL);
       })
     );
-  } else {
-    // For images, CSS, and JS, continue to use the network normally
-    event.respondWith(fetch(event.request));
   }
+  // All other requests (CSS, JS, Images) go straight to network
+  // and are NOT saved to any cache.
 });
